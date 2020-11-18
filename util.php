533f2415aece76865488065e8970dee88bbbb49a
nkanyi73 <nkanyi73@gmail.com>
2020-11-18 08:26:03 +0300

    Commit util.php

---
+++ util.php
@@ -0,0 +1,9 @@
+<?php
+	class Util {			
+        //About DB
+        static $DB_NAME = "newapp";
+        static $DB_USER = "root";
+        static $DB_USER_PASS = "";
+        static $SERVER_NAME = "localhost";
+	}
+?>
